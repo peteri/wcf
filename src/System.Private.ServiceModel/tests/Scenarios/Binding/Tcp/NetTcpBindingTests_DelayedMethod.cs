cd6d1f1bbce4234e799d4e218219607c38c0ad8b
Peter Ibbotson <peter@ibbotson.co.uk>
2019-09-10 15:23:35 +0100

    Added hacky test to show timeout issue

---
+++ src/System.Private.ServiceModel/tests/Scenarios/Binding/Tcp/NetTcpBindingTests_DelayedMethod.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Infrastructure.Common;
+using System.ServiceModel;
+using Xunit;
+
+public partial class Binding_Tcp_NetTcpBindingTests : ConditionalWcfTest
+{
+    // Simple echo of a string using NetTcpBinding on both client and server with all default settings.
+    // Default settings are:
+    //                         - SecurityMode = Transport
+    //                         - ClientCredentialType = Windows
+    [WcfFact]
+    [Condition(nameof(Windows_Authentication_Available))]
+    [OuterLoop]
+    public static void DefaultSettings_Echo_DelayedMethod()
+    {
+        string testString = "Hello";
+        ChannelFactory<IWcfService> factory = null;
+        IWcfService serviceProxy = null;
+
+        try
+        {
+            // *** SETUP *** \\
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.OpenTimeout = TimeSpan.FromMilliseconds(500);
+            factory = new ChannelFactory<IWcfService>(binding, new EndpointAddress(Endpoints.Tcp_DefaultBinding_Address));
+            serviceProxy = factory.CreateChannel();
+            ((ICommunicationObject)serviceProxy).Open();
+            System.Threading.Thread.Sleep(1000);
+            // *** EXECUTE *** \\
+            string result = serviceProxy.Echo(testString);
+
+            // *** VALIDATE *** \\
+            Assert.Equal(testString, result);
+
+            // *** CLEANUP *** \\
+            ((ICommunicationObject)serviceProxy).Close();
+            factory.Close();
+        }
+        finally
+        {
+            // *** ENSURE CLEANUP *** \\
+            ScenarioTestHelpers.CloseCommunicationObjects((ICommunicationObject)serviceProxy, factory);
+        }
+    }
+}
